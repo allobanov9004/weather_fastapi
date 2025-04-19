f2704e5dcee72b9953f37c13cf3f008f96c6ffe7
allobanov <wrider@yandex.ru>
2025-04-19 11:31:43 +0300

    first

---
+++ app.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+import requests
+from pydantic import BaseModel
+import uvicorn
+from config import API_KEY
+
+app = FastAPI()
+
+class WeatherResponse(BaseModel):
+    city: str
+    temperature: float
+    description: str
+
+api_key = API_KEY
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+@app.get("/weather/{city}", response_model=WeatherResponse)
+async def get_weather(city: str):
+    params = {"q": city, "appid": api_key, "units": "metric", "lang": "ru"}
+    response = requests.get(BASE_URL, params=params)
+    
+    if response.status_code != 200:
+        return {"error": "Город не найден или ошибка API"}
+    
+    data = response.json()
+    return WeatherResponse(
+        city=city,
+        temperature=data["main"]["temp"],
+        description=data["weather"][0]["description"]
+    )
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
